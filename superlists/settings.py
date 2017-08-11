c6246587828cb421b98e3f6556c813b1853e7ed7
uidg3913 <paran_piano@hotmail.com>
2017-08-11 16:59:36 +0900

    change database path for server error.

--- superlists/settings.py
+++ superlists/settings.py
@@ -60,7 +60,8 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'NAME': os.path.abspath(os.path.join(BASE_DIR, '../database/db.sqlite3')),
+
     }
 }
 
